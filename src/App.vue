e39a44eadd3fd9bb175d094c717d6d1382abf01e
Viniciusdepaulapalhares <depaulapalharesv@gmail.com>
2023-04-06 11:19:45 -0300

    commit3

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ const nome = ref('');
 const email = ref('');
 const senha = ref('');
 const senhaConfirmacao = ref('');
+const mostrarSenha = ref(false);
 const dataDeNascimento = ref('');
 const endereco = ref(''); 
 const cidade = ref('');
@@ -13,33 +14,33 @@ const liguagensDeProgramacao = ref('');
 const biografia = ref('');
 
 const estados = [
-  { sigla: 'AC', nome: 'Acre' },
-  { sigla: 'AL', nome: 'Alagoas' },
-  { sigla: 'AP', nome: 'Amapá' },
-  { sigla: 'AM', nome: 'Amazonas' },
-  { sigla: 'BA', nome: 'Bahia' },
-  { sigla: 'CE', nome: 'Ceará' },
-  { sigla: 'DF', nome: 'Distrito Federal' },
-  { sigla: 'ES', nome: 'Espírito Santo' },
-  { sigla: 'GO', nome: 'Goiás' },
-  { sigla: 'MA', nome: 'Maranhão' },
-  { sigla: 'MT', nome: 'Mato Grosso' },
-  { sigla: 'MS', nome: 'Mato Grosso do Sul' },
-  { sigla: 'MG', nome: 'Minas Gerais' },
-  { sigla: 'PA', nome: 'Pará' },
-  { sigla: 'PB', nome: 'Paraíba' },
-  { sigla: 'PR', nome: 'Paraná' },
-  { sigla: 'PE', nome: 'Pernambuco' },
-  { sigla: 'PI', nome: 'Piauí' },
-  { sigla: 'RJ', nome: 'Rio de Janeiro' },
-  { sigla: 'RN', nome: 'Rio Grande do Norte' },
-  { sigla: 'RS', nome: 'Rio Grande do Sul' },
-  { sigla: 'RO', nome: 'Rondônia' },
-  { sigla: 'RR', nome: 'Roraima' },
-  { sigla: 'SC', nome: 'Santa Catarina' },
-  { sigla: 'SP', nome: 'São Paulo' },
-  { sigla: 'SE', nome: 'Sergipe' },
-  { sigla: 'TO', nome: 'Tocantins' }
+  { sigla: 'AC', nomeEstado: 'Acre' },
+  { sigla: 'AL', nomeEstado: 'Alagoas' },
+  { sigla: 'AP', nomeEstado: 'Amapá' },
+  { sigla: 'AM', nomeEstado: 'Amazonas' },
+  { sigla: 'BA', nomeEstado: 'Bahia' },
+  { sigla: 'CE', nomeEstado: 'Ceará' },
+  { sigla: 'DF', nomeEstado: 'Distrito Federal' },
+  { sigla: 'ES', nomeEstado: 'Espírito Santo' },
+  { sigla: 'GO', nomeEstado: 'Goiás' },
+  { sigla: 'MA', nomeEstado: 'Maranhão' },
+  { sigla: 'MT', nomeEstado: 'Mato Grosso' },
+  { sigla: 'MS', nomeEstado: 'Mato Grosso do Sul' },
+  { sigla: 'MG', nomeEstado: 'Minas Gerais' },
+  { sigla: 'PA', nomeEstado: 'Pará' },
+  { sigla: 'PB', nomeEstado: 'Paraíba' },
+  { sigla: 'PR', nomeEstado: 'Paraná' },
+  { sigla: 'PE', nomeEstado: 'Pernambuco' },
+  { sigla: 'PI', nomeEstado: 'Piauí' },
+  { sigla: 'RJ', nomeEstado: 'Rio de Janeiro' },
+  { sigla: 'RN', nomeEstado: 'Rio Grande do Norte' },
+  { sigla: 'RS', nomeEstado: 'Rio Grande do Sul' },
+  { sigla: 'RO', nomeEstado: 'Rondônia' },
+  { sigla: 'RR', nomeEstado: 'Roraima' },
+  { sigla: 'SC', nomeEstado: 'Santa Catarina' },
+  { sigla: 'SP', nomeEstado: 'São Paulo' },
+  { sigla: 'SE', nomeEstado: 'Sergipe' },
+  { sigla: 'TO', nomeEstado: 'Tocantins' }
 ];
 </script>
 
@@ -65,11 +66,12 @@ const estados = [
         <div class="form-group">
           <label for="senha">Senha:</label>
           <input type="password" v-model="senha" class="form-control" id="senha" placeholder="Senha" required>
+          <input type="checkbox" v-model="mostrarSenha" value="Sim">
         </div>
 
         <div class="form-group">
           <label for="senha-confirmacao">Confirme a Senha:</label>
-          <input type="password" v-model="senhaConfirmacao" class="form-control" id="senha-confirmacao" placeholder="Confirmação De Senha" required>
+          <input type="password" v-model="senhaConfirmacao" class="form-control" id="senhaConfirmacao" placeholder="Confirmação De Senha" required>
         </div>
 
         <div class="form-group">
@@ -91,7 +93,7 @@ const estados = [
           <label for="estado">Estado:</label>
           <select id="estado" v-model="selectedState" class="form-control" required>
             <option value="" disabled>Selecione um estado</option>
-            <option v-for="(estado, index) in estados" :key="index" :value="estado.sigla">{{ estado.sigla }} - {{ estado.nome }}</option>
+            <option v-for="(estado, index) in estados" :key="index" :value="estado.sigla">{{ estado.sigla }} - {{ estado.nomeEstado }}</option>
           </select>
         </div>
 
@@ -107,9 +109,45 @@ const estados = [
         <label for="biografia">Biografia:</label>
         <textarea v-model="biografia" name="biografia" id="biografia" cols="70" rows="6" class="form-control" placeholder="Biografia"></textarea>
       </div>
+      
       </div>
     </div>
   </div>
+
+    <div class=" card mb-5 mt-5 " id="informacao">
+      <div class="card-header text-center">
+        <h1>Formulário</h1>
+      </div>
+      <div class="card-body">
+        <h2 class="card-title">Seus dados:</h2>
+      <div>
+        <ul class="list-group list-group-flush">
+    <label for="itemNome">Nome:</label>
+    <li class="list-group-item">{{nome}}</li>
+    <label for="itemNome">Email:</label>
+    <li class="list-group-item">{{email}}</li>
+    <label for="itemNome">Senha:</label>
+    <li class="list-group-item">{{senha}}</li>
+    <label for="itemNome">Confirmação:</label>
+    <li class="list-group-item">{{senhaConfirmacao}}</li>
+    <label for="itemNome">Data de Nascimento:</label>
+    <li class="list-group-item">{{dataDeNascimento}}</li>
+    <label for="itemNome">Endereço:</label>
+    <li class="list-group-item">{{endereco}}</li>
+    <label for="itemNome">Cidade:</label>
+    <li class="list-group-item">{{cidade}}</li>
+    <label for="itemNome">Estado:</label>
+    <li class="list-group-item">{{ estados}}</li>
+    <label for="itemNome">Hobbies:</label>
+    <li class="list-group-item">{{hobbies}}</li>
+    <label for="itemNome">Linguagens de Programação:</label>
+    <li class="list-group-item">{{liguagensDeProgramacao}}</li>
+    <label for="itemNome">Biografia:</label>
+    <li class="list-group-item">{{biografia}}</li>
+  </ul>
+</div>
+      </div>
+    </div>
 </template>
 
 <style scoped>
